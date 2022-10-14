a8a9b01cf77e54ce2665291b6327ccd39a44d383
Karol Banat <karol.banat@gmail.com>
2022-10-14 19:24:52 +0200

    Form error message

--- src/js/main.js
+++ src/js/main.js
@@ -95,9 +95,12 @@ const handleFormSubmission = e => {
 	e.preventDefault();
 	const inputText = todoFormInput.value;
 	if (!isBlank(inputText)) {
+		todoForm.classList.remove('error');
 		const newTask = createTaskElement({ id: generateId(), content: inputText });
 		todoList.appendChild(newTask);
 		todoFormInput.value = '';
+	} else {
+		todoForm.classList.add('error');
 	}
 };
 
@@ -110,5 +113,6 @@ const isBlank = value => value === '';
 
 themeToggle.addEventListener('click', handleThemeToggle);
 todoFormSubmit.addEventListener('click', handleFormSubmission);
+todoFormInput.addEventListener('focusin', e => todoForm.classList.remove('error'));
 
 loadTheme();
