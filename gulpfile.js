a2a008ccaa9ed05eb2f90f31875c518b1cb1b18c
Karol Banat <karol.banat@gmail.com>
2022-10-15 13:42:46 +0200

    Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,20 @@ const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
-const babel = require('gulp-babel');
+// const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const clean = require('gulp-clean');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
 const paths = {
 	sass: './src/sass/**/*.scss',
 	js: './src/js/**/*.js',
+	jsEntry: './src/js/main.js',
 	dist: './dist',
 	sassDest: './dist/css',
 	jsDest: './dist/js',
@@ -36,13 +40,18 @@ function sassCompiler(done) {
 }
 
 function javaScript(done) {
-	src(paths.js)
-		.pipe(sourcemaps.init())
-		.pipe(
-			babel({
-				presets: ['@babel/preset-env'],
-			})
-		)
+	browserify({
+		entries: paths.jsEntry,
+		debug: true,
+	})
+		.transform('babelify', {
+			presets: ['@babel/preset-env'],
+			ignore: [/\/node_modules\//],
+		})
+		.bundle()
+		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(sourcemaps.write())
