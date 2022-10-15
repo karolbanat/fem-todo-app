77727f95520bb2c1248dae656e409abadf3bce3f
Karol Banat <karol.banat@gmail.com>
2022-10-15 13:55:26 +0200

    Move elements and theme handling to separate files

---
+++ src/js/elements.js
@@ -0,0 +1,7 @@
+const themeToggle = document.querySelector('.theme-toggle');
+const todoList = document.querySelector('.todo-list');
+const todoForm = document.querySelector('.todo-form');
+const todoFormInput = todoForm.querySelector('.todo-form__input');
+const todoFormSubmit = todoForm.querySelector('button[type=submit]');
+
+export { themeToggle, todoList, todoForm, todoFormInput, todoFormSubmit };
---
+++ src/js/theme.js
@@ -0,0 +1,35 @@
+import { themeToggle } from './elements';
+
+const loadTheme = () => {
+	const theme = localStorage.getItem('theme');
+	if (theme) {
+		document.body.setAttribute('data-theme', theme);
+		themeToggle.dataset.theme = theme;
+	} else {
+		loadPreferedTheme();
+	}
+	return theme;
+};
+
+const loadPreferedTheme = () => {
+	const preferedTheme =
+		window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+	changeColorTheme(preferedTheme);
+	return preferedTheme;
+};
+
+const handleThemeToggle = e => {
+	const currentTheme = themeToggle.dataset.theme;
+	const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+	changeColorTheme(newTheme);
+	return newTheme;
+};
+
+const changeColorTheme = theme => {
+	themeToggle.dataset.theme = theme;
+	document.body.setAttribute('data-theme', theme);
+	localStorage.setItem('theme', theme);
+	return theme;
+};
+
+export { loadTheme, handleThemeToggle };
